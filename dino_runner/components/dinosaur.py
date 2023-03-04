87b93421a8131fe68c95ceaeb43ab458fecbca23
AdileneVillamil2001 <adilene.villamil2001@gmail.com>
2023-03-04 03:28:51 +0100

    clase 2: dinosaur

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,33 +1,46 @@
 import pygame
-BLACK = (0, 0, 0)
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import RUNNING
+from dino_runner.utils.constants import DUCKING
 
-class Dinosaur(pygame.sprite.Sprite):
+
+class Dinosaur(Sprite):
+    POS_X = 80
+    POS_Y = 310
+
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("dino_runner/assets/Dino/DinoRun1.png")
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.top, self.rect.left = 300, 200
-    def update(self, surface):
-        surface.blit(self.image, self.rect)
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_Y
+        self.step_index = 0
+    def update(self, user_input):
+        if user_input[pygame.K_DOWN]:
+            self.duck()
+        else:
+             self.run()
+
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def draw(self, screen):
+        screen.blit (self.image, self.dino_rect)
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_Y
+        self.step_index += 1
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = 350
+        self.step_index += 1
+
+
         
-        
-pygame.init()
-
-screen = pygame.display.set_mode([1100, 600])
-clock = pygame.time.Clock()
-
-done = False
-
-player = Dinosaur()
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-    
-    screen.fill((255, 255, 255))
-    player.update(screen)
-    pygame.display.flip()
-    clock.tick(60)
-
-pygame.quit()
