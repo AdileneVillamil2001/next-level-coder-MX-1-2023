088d685675144719853def16941961755f27a0c3
AdileneVillamil2001 <adilene.villamil2001@gmail.com>
2023-03-08 02:26:09 +0100

    clase 3 homewrok bird

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,9 +1,15 @@
-import random
 from dino_runner.components.obstacles.obstacle import Obstacle
-from dino_runner.utils.constants import BIRD
 
 class Bird(Obstacle):
       def __init__(self, image):
-        self.type = random.randint(1, 1)
+        self.type = 0
         super().__init__(image, self.type)
         self.rect.y = 100
+        self.step_index = 0
+      
+      def draw(self, SCREEN):
+          if self.step_index >= 10:
+            self.step_index = 0
+
+          SCREEN.blit(self.image[self.step_index <5 ],self.rect)
+          self.step_index += 1
